64627a8a0105ec94ce0d9365213213fb6f5e112c
Benjamin Thomas <benjamin@thomasnetwork.net>
2021-01-03 16:00:13 -0500

    Setup mock data

--- src/server/routes.ts
+++ src/server/routes.ts
@@ -1,16 +1,20 @@
 import * as express from "express";
 import pdfReader from "./pdfReader";
+import  { promises } from "fs";
 
 const router = express.Router();
 
-router.get("/api/hello", (req, res) => {
-  res.json("World");
+router.get("/api/fake-data", async (req, res) => {
+  const result = await promises.readFile("/home/ben/Documents/11-2020.json", "utf-8");
+  console.log(result);
+  const data = JSON.parse(result);
+  res.json(data);
 });
 
 router.get("/api/import", async (req, res) => {
   const data = await pdfReader("/home/ben/Downloads/10306_201130092911_4739580054117.PDF");
 
-  res.json(data);
+  res.send(data);
 });
 
 export default router;
